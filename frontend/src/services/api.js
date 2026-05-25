620aea934e1d3b9e2c9fe05d808ec1f3474fc719
Gowri <gowri.challa@rm1codershub.com>
2026-05-25 18:37:31 +0530

    updated api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://breathe-esg-assignment-jxdc.onrender.com/api",
 });
 
 export default API;
